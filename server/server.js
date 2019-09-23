207bdbf67e5a0cf9c15bc5dbdf7581cb58cc8b09
arturesteves <arturmesteves@hotmail.com>
2019-09-23 11:59:25 +0100

    Add Todo Item

--- server/server.js
+++ server/server.js
@@ -11,29 +11,58 @@ const server = Hapi.server({
       provider: {
         constructor: CatboxMemory,
         options: {
-          partition : 'todo_list_cached_data',
+          partition: 'todo_list_cached_data',
         }
       }
     }
   ]
 });
 
-const Cache = server.cache({ segment: 'todo_list', expiresIn: 1000 * 5 });
+const Cache = server.cache({segment: 'todo_list', expiresIn: 1000 * 5});
+const cacheSegmenet = 'todos';
 
 server.route({
-  method: 'GET',
-  path: '/',
+  method: 'PUT',
+  path: '/todos',
   handler: async (request, h) => {
+    try {
+      const payload = request.payload;
+      const { description } = payload;
+      console.log('description', description);
 
+      const newTodoItem = {
+        id: generateRandomUniqueIdentifier().toString(),
+        state: 'INCOMPLETE',
+        description,
+        dateAdded: new Date().toISOString()
+      };
+      console.log(`New Todo Item: ${ JSON.stringify(newTodoItem) }`);
+
+      const key = {
+        segment: cacheSegmenet,
+        id: newTodoItem.id
+      };
+
+      await Cache.set(key, newTodoItem, 5000);
+      console.log('Todo Item saved');
+
+      return newTodoItem;
+    }catch(e){
+      console.log(e);
+      return e;
+    }
   }
 });
+
+const generateRandomUniqueIdentifier = () => {
+  return Math.floor(Math.random() * 10000) + 1;
+};
 
 const start = async function () {
 
   try {
     await server.start();
-  }
-  catch (err) {
+  } catch (err) {
     console.log(err);
     process.exit(1);
   }
