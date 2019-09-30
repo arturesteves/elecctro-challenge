7b51b3f3e06dfd243529b5913eddb3d97c06acee
arturesteves <arturmesteves@hotmail.com>
2019-09-30 08:32:59 +0100

    Show Todo View Component when Listing Todo Items

--- web/src/ListTodoItems.jsx
+++ web/src/ListTodoItems.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as PropTypes from 'prop-types';
+import ViewTodoItem from "./ViewTodoItem";
 
-const ListTodoItems = ({ todos }) => {
+
+const markTodo = (e) => {
+	console.log(e.target.checked);
+
+};
+
+const ListTodoItems = ({ todos: todoList }) => {
+	const [ todos, setTodos ] = useState([]);
+
+	useEffect(() => {
+		setTodos(todoList);
+	}, []);
+
 	return (
 		<div>
 			<ul>
-				{ todos.map((todo) => {
-					return <li>{ todo.description }</li>;
+				{ todos.map((todo, index) => {
+					return <li key={ index }>
+						<ViewTodoItem todo={todo}/>
+					</li>;
 				}) }
 			</ul>
 		</div>
