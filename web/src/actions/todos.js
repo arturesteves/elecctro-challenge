b21c98975514c97bf6cf0a3f7b33c83e731a72a4
arturesteves <arturmesteves@hotmail.com>
2019-10-04 07:55:57 +0100

    Define Edit and Delete Todo Redux Actions

--- web/src/actions/todos.js
+++ web/src/actions/todos.js
@@ -1,4 +1,4 @@
-import { addTodoItemRequest, getTodoItemsRequest } from "../services/todos";
+import { addTodoItemRequest, deleteTodoItemRequest, editTodoItemRequest, getTodoItemsRequest } from "../services/todos";
 
 
 export const ADD_TODO = 'ADD_TODO';
@@ -11,7 +11,7 @@ const addTodoAction = (newTodo) => {
 export const addTodo = (todo) => {
 	return async (dispatch) => {
 		const result = await addTodoItemRequest(todo);
-		if(result.error) {
+		if (result.error) {
 			console.log('Add Todo Request Failed');
 			return;
 		}
@@ -31,10 +31,47 @@ export const fetchTodos = () => {
 	return async (dispatch) => {
 		const result = await getTodoItemsRequest();
 		console.log('Result', result);
-		if(result.error) {
+		if (result.error) {
 			console.log('Fetch Todos Request Failed');
 			return;
 		}
 		dispatch(fetchTodosAction(result.data));
 	}
 };
+
+export const EDIT_TODO = 'EDIT_TODO';
+export const editTodoAction = (todo) => {
+	return {
+		type: EDIT_TODO,
+		todo
+	}
+};
+export const editTodo = (todo) => {
+	return async (dispatch) => {
+		const result = await editTodoItemRequest(todo);
+		if (result.error) {
+			console.log('Edit Todo Request Failed');
+			return;
+		}
+		dispatch(editTodoAction(result.data));
+	}
+};
+
+export const DELETE_TODO = 'DELETE_TODO';
+export const deleteTodoAction = (id) => {
+	return {
+		type: DELETE_TODO,
+		id
+	}
+};
+export const deleteTodo = (id) => {
+	return async (dispatch) => {
+		const result = await deleteTodoItemRequest(id);
+		if (result.error) {
+			console.log('Delete Todo Request Failed');
+			return;
+		}
+		dispatch(deleteTodoAction(result.data));
+	}
+};
+
