bce943773564b3c5a2932532e3ac4f14b9fbe767
arturesteves <arturmesteves@hotmail.com>
2019-10-14 11:38:40 +0100

    Display Incomplete Todo List Item with Material UI Components

--- web/src/components/ViewTodoItem.jsx
+++ web/src/components/ViewTodoItem.jsx
@@ -1,16 +1,24 @@
 import React, { Fragment, useState } from 'react';
 import * as PropTypes from 'prop-types';
 import EditTodoItem from "./EditTodoItem";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Checkbox from "@material-ui/core/Checkbox";
 
 
 const ViewTodoItem = ({ todo, onDelete, onEdit, displayNotification }) => {
 	const [ mode, setMode ] = useState('VIEW');
 
 	return (
-		<Fragment>
+		<ListItem role={ undefined } dense button>
 			{ mode === 'VIEW' ? showViewMode(todo, onDelete, onEdit, setMode, displayNotification) : null }
 			{ mode === 'EDIT' ? showEditMode(todo, onEdit, setMode, displayNotification) : null }
-		</Fragment>
+		</ListItem>
 	);
 };
 
@@ -59,25 +67,38 @@ const ViewCompleteTodoItem = ({ todo, onDelete, displayNotification }) => {
 };
 
 const ViewIncompleteTodoItem = ({ todo, onDelete, onEdit, setMode, displayNotification }) => {
+	const labelId = `checkbox-list-label-${ todo.id }`;
 
 	return (
 		<Fragment>
-			<input type="checkbox" name="todoCompleted" onChange={ (e) => {
-				updateTodoState(e.target.value, todo, onEdit, displayNotification);
-			} }/>
-			<span>{ todo.description }{ ' ' }</span>
-			<button onClick={ () => {
-				setMode('EDIT');
-			} }>Edit
-			</button>
-			<button onClick={ () => {
-				onDelete(() => {
-					displayNotification(successNotification(<p>Todo Deleted with Success</p>));
-				}, () => {
-					displayNotification(failureNotification(<p>We could not delete your Todo, Something happened</p>));
-				});
-			} }>Delete
-			</button>
+			<ListItemIcon>
+				<Checkbox
+					edge="start"
+					onChange={ (e) => {
+						updateTodoState(e.target.checked, todo, onEdit, displayNotification);
+					} }
+					tabIndex={ -1 }
+					disableRipple
+					inputProps={ { 'aria-labelledby': labelId } }
+				/>
+			</ListItemIcon>
+			<ListItemText id={ labelId } primary={ todo.description }/>
+			<ListItemSecondaryAction>
+				<IconButton edge="end" aria-label="edit" onClick={ () => {
+					setMode('EDIT');
+				} }>
+					<EditIcon/>
+				</IconButton>
+				<IconButton edge="end" aria-label="delete" onClick={ () => {
+					onDelete(() => {
+						displayNotification(successNotification(<p>Todo Deleted with Success</p>));
+					}, () => {
+						displayNotification(failureNotification(<p>We could not delete your Todo, Something happened</p>));
+					});
+				} }>
+					<DeleteIcon color={ "secondary" }/>
+				</IconButton>
+			</ListItemSecondaryAction>
 		</Fragment>
 	)
 };
