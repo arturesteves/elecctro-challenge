0d4e42d91607199092a51204c3977af989a3bbd7
arturesteves <arturmesteves@hotmail.com>
2019-10-14 11:39:14 +0100

    Display Todo List Item in Edit Mode with Material UI Components

--- web/src/components/EditTodoItem.jsx
+++ web/src/components/EditTodoItem.jsx
@@ -1,4 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import SaveIcon from '@material-ui/icons/Save';
+import CancelIcon from '@material-ui/icons/Cancel';
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 
 
 const EditTodoItem = (props) => {
@@ -8,22 +13,42 @@ const EditTodoItem = (props) => {
 		setDescription(props.todo.description);
 	}, []);
 
+	const labelId = `checkbox-list-label-${ props.todo.id }`;
+
 	return (
-		<form name="editTodoForm" onSubmit={ (e) => {
-			e.preventDefault();
-			onSubmit(e.target.todoDescription.value, props.todo, props.onEdit)
-		} }>
-			<input type="text" name="todoDescription" placeholder="Write new task here" required={ true }
-						 value={ description }
-						 onChange={ (e) => {
-							 setDescription(e.target.value);
-						 } }/>
-			<button type="submit">Save</button>
-			<button type="button" onClick={ () => {
-				props.onCancel();
-			} }>Cancel
-			</button>
-		</form>
+		<Fragment>
+				<TextField
+					id={labelId}
+					type="text"
+					label="Description"
+					style={ { margin: 8, marginRight: 80 } }
+					placeholder="Write a new task here"
+					required
+					fullWidth
+					margin="normal"
+					InputLabelProps={ {
+						shrink: true,
+					} }
+					value={ description }
+					onChange={ (e) => {
+						setDescription(e.target.value);
+					} }
+				/>
+
+			<ListItemSecondaryAction>
+				<IconButton type="submit" edge="end" aria-label="save" onClick={ (e) => {
+					onSubmit(description, props.todo, props.onEdit)
+				} }>
+					<SaveIcon/>
+				</IconButton>
+
+				<IconButton edge="end" aria-label="delete" onClick={ () => {
+					props.onCancel();
+				} }>
+					<CancelIcon color={ "secondary" }/>
+				</IconButton>
+			</ListItemSecondaryAction>
+		</Fragment>
 	)
 };
 
