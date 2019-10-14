24bea6f561ae3be7d0f66c9dacbb6b1e3abdd206
arturesteves <arturmesteves@hotmail.com>
2019-10-14 10:45:15 +0100

    Header with Elecctro Logo

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -4,11 +4,13 @@ import AddTodoItem from "./AddTodoItem";
 import ListTodoItemsContainer from "../containers/ListTodoItemsContainer";
 import ControlTodoListItemsVisibility from "../containers/ControlTodoListItemsVisibility";
 import AddTodoItemContainer from "../containers/AddTodoItemContainer";
+import Header from "./Header";
 
 
 const App = () => {
 	return (
 		<div className="App">
+			<Header/>
 			<AddTodoItemContainer/>
 			<ListTodoItemsContainer/>
 			<ControlTodoListItemsVisibility/>
