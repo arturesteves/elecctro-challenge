c6adcb2625a33cba7ebd487f5763370f3c45b517
arturesteves <arturmesteves@hotmail.com>
2019-10-16 11:57:29 +0100

    Change Font Family of Header

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -2,19 +2,10 @@ import React from 'react';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
 import ElecctroLogo from '../resources/logo-original-red.png';
+import Box from "@material-ui/core/Box";
+
 
 const useStyles = makeStyles(theme => ({
 	grow: {
@@ -79,20 +70,22 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
-const Header = (props) => {
+const Header = () => {
 	const classes = useStyles();
 
 	return (
-		<div className={classes.grow}>
+		<div className={ classes.grow }>
 			<AppBar position="static">
 				<Toolbar>
-						<img src={ElecctroLogo}
-								 className={classes.menuButton}
-								 color="inherit"
-								 aria-label="open drawer"
-								 height="80" width="130	"/>
-					<Typography className={classes.title} variant="h6" noWrap>
-						Todo Web App - Elecctro React Challenge
+					<img src={ ElecctroLogo }
+							 className={ classes.menuButton }
+							 color="inherit"
+							 aria-label="open drawer"
+							 height="80" width="130	"/>
+					<Typography className={ classes.title } variant="h6" noWrap>
+						<Box fontFamily="Monospace">
+							Todo Web App - Elecctro React Challenge
+						</Box>
 					</Typography>
 				</Toolbar>
 			</AppBar>
