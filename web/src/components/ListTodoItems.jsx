a57c892657f96be0595703455d052ecb7dcd4dc1
arturesteves <arturmesteves@hotmail.com>
2019-10-16 10:15:38 +0100

    Add Styles to List Component

--- web/src/components/ListTodoItems.jsx
+++ web/src/components/ListTodoItems.jsx
@@ -3,16 +3,33 @@ import * as PropTypes from 'prop-types';
 import ClickableHeader from "../containers/ClickableHeader";
 import ViewTodoItemContainer from "../containers/ViewTodoItemContainer";
 import List from "@material-ui/core/List";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		width: "80%",
+		margin: "0 auto",
+	},
+}));
 
 const ListTodoItems = ({ todos }) => {
+	const classes = useStyles();
 
 	return (
 		<div>
-			<ClickableHeader>Tasks</ClickableHeader>
+			<ClickableHeader>
+				<Typography component="div">
+					<Box fontSize="h5.fontSize" letterSpacing={ 6 } m={ 1 }>
+						Tasks
+					</Box>
+				</Typography>
+			</ClickableHeader>
 			{ todos.length === 0 ?
 				<span>No Todos Found</span> :
-				<List>
+				<List className={ classes.root }>
 					{ todos.map((todo, index) => {
 						return <ViewTodoItemContainer key={ index } todo={ todo } index={ index }/>;
 					}) }
