76d62bfd58884cacc9843b90d9785a38b7304c61
arturesteves <arturmesteves@hotmail.com>
2019-10-17 16:40:21 +0100

    Sync Global State with the Browser's Local Storage

--- web/src/store.js
+++ web/src/store.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, createStore } from "redux";
-import reducers from "./reducers";
 import thunk from "redux-thunk";
 import { combineReducers } from 'redux-immutable';
 import { Map } from 'immutable';
-
+import * as storage from 'redux-storage'
+import createEngine from 'redux-storage-engine-localstorage';
+import reducers from "./reducers";
 
 const middlewares = [ thunk ];
 
@@ -12,6 +13,10 @@ if (process.env.NODE_ENV !== `production`) {
 	middlewares.push(logger);
 }
 
+const engine = createEngine('elecctro-todo-storage-key');
+const storageMiddleware = storage.createMiddleware(engine, [], []);
+middlewares.push(storageMiddleware);
+
 const rootReducer = combineReducers(reducers);
 
 const store = createStore(
@@ -19,5 +24,11 @@ const store = createStore(
 	Map({}),
 	applyMiddleware(...middlewares)
 );
+
+const load = storage.createLoader(engine);
+
+load(store)
+	.then((newState) => console.log('Loaded state:', newState))
+	.catch(() => console.log('Failed to load previous state'));
 
 export default store;
